d7d5686123ff86f9e6d4cfcd8cddef98e139be9f
sachinvarmaiitg <134069846+sachinvarmaiitg@users.noreply.github.com>
2024-06-09 16:52:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app=express();
 const cors=require('cors');
+const session=require('express-session');
+const MongoStore = require('connect-mongo');
 const bodyParser=require('body-parser')
 const dbURL=process.env.ATLAS_URL;
 const corsOptions ={
@@ -15,6 +17,31 @@ const cookieParser = require('cookie-parser');
 const adminRouter=require("./routes/admin");
 const userRouter=require("./routes/userTrip");
 const paymentRouter=require("./routes/payment");
+const store=MongoStore.create({
+  mongoUrl:process.env.MONGODB_URI,
+  touchAfter:24*3600,
+  crypto:{
+    secret:"mysupersecretcode",
+  }
+})
+const sessionOptions={
+  store:store,
+  secret:"mysupersecretcode",
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    expires:Date.now()+7*24*3600*1000,
+    maxAge:7*24*3600*1000,
+    httpOnly:true
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session(sessionOptions));
 main(
     console.log("connection succeed")
 ).catch(err => console.log(err));
@@ -23,13 +50,6 @@ main(
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI);
 }
-
-app.use(cors(corsOptions));
-app.use(express.urlencoded({extended:true}));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
 
 app.post("/gettoken",isLoggedIn)
 app.use("/admin",adminRouter)
