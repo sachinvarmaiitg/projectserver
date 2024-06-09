55cb925bfdf83a0e7971881266782b48312fdeca
sachin28 <Sachinvarmaiitjee@gmail.com>
2024-06-09 22:13:55 +0530

    v4

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const router=express.Router();
-const { adminAuthMiddleware } = require('../middlewares/isAuth');
+const { adminAuthMiddleware, isauthMiddleware } = require('../middlewares/isAuth');
 const AdminController=require("../controllers/AdminController");
 
 router.post("/addflight", adminAuthMiddleware,AdminController.AddFlight);
 
-router.get("/allusers",adminAuthMiddleware,AdminController.GetUsers);
+router.get("/allusers",AdminController.GetUsers);
 
 router.get('/activeflights',AdminController.GetActiveFlights);
 
