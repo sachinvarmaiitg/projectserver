5fc17107e4aad5d5d518ce5ca0b4fa252d203f92
sachin28 <Sachinvarmaiitjee@gmail.com>
2024-06-09 22:52:41 +0530

    v5.1

--- config/parseDateTime.js
+++ config/parseDateTime.js
@@ -1,6 +1,8 @@
 function parseDateTime(dateString, timeString) {
     const dateTimeString = `${dateString}T${timeString}`;
-  return new Date(dateTimeString);
+  const date = new Date(dateTimeString);
+  return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
 }
+
 
 module.exports = parseDateTime;
